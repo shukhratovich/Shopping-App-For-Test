2c67d1b62279754fe9f4ac0cbc32df3be285212d
Asomiddin <shaydulov1996@gmail.com>
2024-10-24 10:34:49 +0500

    added libriry room and navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -45,4 +46,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
+
+    implementation("androidx.navigation:navigation-fragment:2.5.0")
+    implementation("androidx.navigation:navigation-ui:2.5.0")
+
 }
